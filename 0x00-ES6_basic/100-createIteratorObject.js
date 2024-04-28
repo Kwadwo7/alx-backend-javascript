4b458dbf8019604c86afcd9dba185589e80d417a
Kwadwo7 <appiahbenedict724@gmail.com>
2024-04-28 00:18:06 -0700

    updated ES6 basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,14 +1,9 @@
-// The Breakdown 😏😉😉
-// // I know it's long, I was just trying to understand how things work
-// //
-// // Here's the shorter code using generator function
-// //
 export default function createIteratorObject(report) {
   return (function* _() {
-     for (const department of Object.values(report.allEmployees)) {
-         for (const employee of department) {
-		 yield employee;
-              }
-           } 
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
   }());
 }
